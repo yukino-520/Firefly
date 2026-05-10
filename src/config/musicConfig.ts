cbb7f787342938ad8e49e016fbc8ddd54c256a19
yukino-520 <2931247743@qq.com>
2026-05-10 14:42:55 +0800

    fix(音乐配置): 更新Meting API地址为自定义地址

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -26,7 +26,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	meting: {
 		// Meting API 地址
 		// 默认使用官方 API，也可以使用自定义 API
-		api: "https://api.i-meto.com/meting/api?server=:server&type=:type&id=:id&r=:r",
+		api: "https://meting.naihee.com/api?server=:server&type=:type&id=:id&r=:r",
 		// 音乐平台：netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 		server: "netease",
 		// 类型：song=单曲, playlist=歌单, album=专辑, search=搜索, artist=艺术家
