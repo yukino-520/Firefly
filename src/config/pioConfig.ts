82ad7b71106fce3693172ae87fe8b912afb55ce6
yukino-520 <2931247743@qq.com>
2026-05-11 09:12:27 +0800

    fix: 在移动端默认隐藏live2d模型

--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -130,7 +130,7 @@ export const live2dModelConfig: Live2DModelConfig = {
 	// 响应式配置
 	responsive: {
 		// 在移动端隐藏
-		hideOnMobile: false,
+		hideOnMobile: true,
 		// 移动端断点
 		mobileBreakpoint: 768,
 	},
